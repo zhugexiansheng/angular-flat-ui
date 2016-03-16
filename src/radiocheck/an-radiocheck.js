1f2f29a7e332b56fa5bc247ab2058d05c53ebec8
zxm107999 <zxm107999@alibaba-inc.com>
2016-03-16 14:33:51 +0800

    修改radiocheck，使其使用方式和原生的angular类似

--- src/radiocheck/an-radiocheck.js
+++ src/radiocheck/an-radiocheck.js
@@ -18,20 +18,43 @@
 			restrict: 'AE',
 			require: '?ngModel',
 			link: function(scope, elem, attr, ngModel){
-				var colorStyle = attr.radioType || 'green';
+				var colorStyle = attr.radioType || 'green',
+					inputType = attr.type,
+					renderFn,
+					toggleFn;
 				elem.iCheck({
 					checkboxClass: 'icheckbox_square-'+colorStyle,
 				    radioClass: 'iradio_square-'+colorStyle,
 				    increaseArea: '30%' // optional
 				});
 
-				if(ngModel){
-					ngModel.$render = function(){
+				if(inputType=="radio"){
+					renderFn = function(){
+						elem.iCheck((ngModel.$viewValue===attr.value)?"check":'uncheck');
+					};
+
+					toggleFn = function(){
+						if(elem.is(":checked")){
+							ngModel.$setViewValue(attr.value);
+						}
+					};
+				}else if (inputType=="checkbox") {
+					renderFn = function(){
 						elem.iCheck(ngModel.$viewValue?'check':'uncheck');
 					};
 
-					elem.on('ifToggled', function(e){
+					toggleFn = function(){
 						ngModel.$setViewValue(elem.is(":checked"));
+					};
+				}
+
+				if(ngModel){
+					ngModel.$render = function(){
+						renderFn();
+					};
+
+					elem.on('ifToggled', function(e){
+						toggleFn();
 					});
 				}
 			}
